17437bd09164163af492d1695d4c67744a24a8ed
Esauh <esau.hutcherson@bison.howard.edu>
2022-04-11 17:19:24 -0400

    Commit
    added comments and changed error with line 14 being la $a0 as it needs to be $s0 so the user input is saved into this value.  jal length is utilized to go the length sub program later to be implemented which should go and get the length of the characters entered.  The length of the character is stored into $s1 and the adress of the last char in the string is saved into $s2.

--- Project 2/project2.s
+++ Project 2/project2.s
@@ -8,6 +8,13 @@ error: .asciiz "Not Recognized"
 
 main:
 li $v0, 8
-la $a0, input_space
+la $a0, input_space #user input
 li $a1, 1001
 syscall
+la $s0, input_space #saves user input into $s0 to be utilized
+
+jal length		# saves the return address of the instruction and jumps to specific instruction
+addi $s1, $v0, 0
+addi $s2, $v1, 0
+
+
